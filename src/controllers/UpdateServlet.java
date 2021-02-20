4fb9ff1d6d5f6a6dc15bddb051b5c0162dc99583
eishin.nishida <have0322eishin@gmail.com>
2021-02-21 04:16:18 +0900

    Add validation

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
 import utils.DBUtil;
+import validators.MessageValidator;
 
 /**
  * Servlet implementation class UpdateServlet
@@ -49,7 +52,20 @@ public class UpdateServlet extends HttpServlet {
             m.setContent(content);
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
+            m.setUpdated_at(currentTime);
+
+            List<String> errors = MessageValidator.validate(m);
+            if(errors.size() > 0) {
+                em.close();
+
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("message", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+                rd.forward(request, response);
+            } else {// 更新日時のみ上書き
 
             // データベースを更新
             em.getTransaction().begin();
@@ -67,7 +83,7 @@ public class UpdateServlet extends HttpServlet {
         }
     }
 }
-
+}
 
 
 
